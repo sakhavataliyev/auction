1dde119aa2e279a60fa36b5ee6a04b9acb9726f0
Sakhavat Aliyev <sakhavataliyevs@gmail.com>
2023-09-04 22:37:18 +0400

    Added mobile menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -46,13 +46,20 @@
           <ol class="breadcrumb bg-transparent mb-1 pb-0 pt-1 px-0 me-sm-6 me-5">
           </ol>
         </nav>
+
+
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
           </div>
           <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item dropdown pe-2 d-flex align-items-center">
-              <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
-              </ul>
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                </div>
+              </a>
             </li>
           </ul>
         </div>
